fa4d0f3f37f93201036da03dc87c002f1a1ba4d3
rocket <adamrivard87@gmail.com>
2025-07-25 20:42:36 -0400

    d

--- build_files/build.sh
+++ build_files/build.sh
@@ -12,7 +12,7 @@ set -ouex pipefail
 # this installs a package from fedora repos
 dnf5 install -y yad
 
-flatpak install -y flathub com.raggesilver.BlackBox
+flatpak install -y flathub com.discordapp.Discord
 
 # Use a COPR Example:
 #
